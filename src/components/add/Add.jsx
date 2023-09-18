6505e40c0b891dabb6f1c98b48f4154abf71dcac
k1k3cb <enriquecbar@gmail.com>
2023-09-19 00:44:52 +0200

    users and products page

--- src/components/add/Add.jsx
+++ src/components/add/Add.jsx
@@ -1,7 +1,32 @@
+import { v4 } from 'uuid';
 import './add.scss';
 
-const Add = () => {
-	return <div className='add'>Add</div>;
+const Add = ({ slug, columns, setOpen }) => {
+	const handleSubmit = e => {
+		e.preventDefault();
+	};
+
+	return (
+		<div className='add'>
+			<div className='modal'>
+				<div className='close' onClick={() => setOpen(false)}>
+					X
+				</div>
+				<h1>Add new {slug}</h1>
+				<form onSubmit={handleSubmit}>
+					{columns
+						.filter(item => item.field !== 'id' && item.field !== 'img')
+						.map(column => (
+							<div className='item' key={v4()}>
+								<label htmlFor=''>{column.headerName}</label>
+								<input type={column.type} placeholder={column.field} />
+							</div>
+						))}
+					<button>Send</button>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Add;
